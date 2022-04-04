2f38ab155623430b76f41028b9de4c0b8952c300
Eduardo Maturana <edu@Mac-mini-de-Eduardo.local>
2022-04-03 21:20:55 -0400

    create order handlers

--- internals/order/core/domain/domain.go
+++ internals/order/core/domain/domain.go
@@ -2,15 +2,15 @@ package domain
 
 type Order struct {
 	ID            string    `json:"id"`
-	CustomerEmail string    `json:"customer_email"`
-	Products      []Product `json:"products"`
-	Status        string    `json:"status" enum:"pending,confirmed,rejected"`
+	CustomerEmail string    `json:"customer_email" validate:"required, email"`
+	Products      []Product `json:"products" validate:"required"`
+	Status        string    `json:"status" enum:"pending,confirmed,rejected" validate:"required"`
 	CreatedAt     string    `json:"created_at"`
 }
 
 type Product struct {
 	ID       string `json:"id"`
-	Name     string `json:"name"`
-	Price    uint16 `json:"price"`
-	Quantity uint8  `json:"quantity"`
+	Name     string `json:"name" validate:"required"`
+	Price    uint16 `json:"price" validate:"required,gte=0"`
+	Quantity uint8  `json:"quantity" validate:"required,gte=1"`
 }
