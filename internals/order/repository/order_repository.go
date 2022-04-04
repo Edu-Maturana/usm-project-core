5749cfd237c52f15e8c414541631c6f898dd0213
Eduardo Maturana <edu@Mac-mini-de-Eduardo.local>
2022-04-03 21:10:02 -0400

    create order repository

--- internals/order/repository/order_repository.go
+++ internals/order/repository/order_repository.go
@@ -1,1 +1,80 @@
 package repository
+
+import (
+	"back-usm/internals/order/core/domain"
+	"log"
+
+	"github.com/fatih/color"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+)
+
+type OrderRepository struct {
+	dsn string
+	db  *gorm.DB
+}
+
+func NewOrderRepository(dsn string) *OrderRepository {
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{
+		Logger: logger.Default.LogMode(logger.Silent),
+	})
+
+	if err != nil {
+		panic(err)
+	}
+
+	log.Print(color.GreenString("Connected to database"))
+	return &OrderRepository{
+		dsn: dsn,
+		db:  db,
+	}
+}
+
+func (r *OrderRepository) GetAll() ([]domain.Order, error) {
+	var orders []domain.Order
+	err := r.db.Find(&orders).Error
+	if err != nil {
+		return orders, err
+	}
+
+	return orders, nil
+}
+
+func (r *OrderRepository) GetOne(id string) (domain.Order, error) {
+	var order domain.Order
+	err := r.db.Where("id = ?", id).First(&order).Error
+	if err != nil {
+		return order, err
+	}
+
+	return order, nil
+}
+
+func (r *OrderRepository) Create(order domain.Order) (domain.Order, error) {
+	err := r.db.Create(&order).Error
+	if err != nil {
+		return order, err
+	}
+
+	return order, nil
+}
+
+func (r *OrderRepository) Update(id string, order domain.Order) (domain.Order, error) {
+	err := r.db.Model(&order).Where("id = ?", id).Updates(&order).Error
+	if err != nil {
+		return order, err
+	}
+
+	return order, nil
+}
+
+func (r *OrderRepository) Delete(id string) error {
+	err := r.db.Where("id = ?", id).Delete(&domain.Order{}).Error
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
