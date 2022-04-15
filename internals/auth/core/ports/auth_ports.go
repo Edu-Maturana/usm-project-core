2f8254b84ac944f0bd0dbb9d5e36f17c08cc63ee
Eduardo Maturana <edu@Mac-mini-de-Eduardo.local>
2022-04-14 21:40:02 -0400

    activate account added

--- internals/auth/core/ports/auth_ports.go
+++ internals/auth/core/ports/auth_ports.go
@@ -8,12 +8,15 @@ import (
 
 type AuthRepository interface {
 	Login(admin domain.Admin) (domain.Admin, error)
+	ActivateAccount(admin domain.Admin) (domain.Admin, error)
 }
 
 type AuthServices interface {
 	Login(admin domain.Admin) (domain.Admin, error)
+	ActivateAccount(admin domain.Admin) (domain.Admin, error)
 }
 
 type AuthHandlers interface {
 	Login(ctx *fiber.Ctx) error
+	ActivateAccount(admin domain.Admin) (domain.Admin, error)
 }
