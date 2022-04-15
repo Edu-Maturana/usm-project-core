83429345474db39f4cff325cc8dd316c2b36ad71
Eduardo Maturana <edu@Mac-mini-de-Eduardo.local>
2022-04-14 21:36:56 -0400

    create auth ports

---
+++ internals/auth/core/ports/auth_ports.go
@@ -0,0 +1,19 @@
+package ports
+
+import (
+	"back-usm/internals/auth/core/domain"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+type AuthRepository interface {
+	Login(admin domain.Admin) (domain.Admin, error)
+}
+
+type AuthServices interface {
+	Login(admin domain.Admin) (domain.Admin, error)
+}
+
+type AuthHandlers interface {
+	Login(ctx *fiber.Ctx) error
+}
